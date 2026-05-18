d8bccdb6cc9d06fe1f456d949001fd4d88d879c0
vicky <pratamavr@gmail.com>
2026-05-19 06:23:36 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Modules\UnitConverter\Http\Controllers\Api\UnitController;
 
 Route::middleware(['auth:sanctum'])->prefix('units')->group(function () {
   Route::get('all', [UnitController::class, 'index']);
-  Route::get('convert', [UnitController::class, 'convert']);
+  Route::post('convert', [UnitController::class, 'convert']);
 });
