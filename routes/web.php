f95c064cf8623b520fe05d4bb993ff7fdc481588
vicky <pratamavr@gmail.com>
2026-05-19 02:14:34 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\UnitConverter\Http\Controllers\UnitConverterController;
 
-Route::middleware(['auth'])->prefix('apps')->group(function () {
+Route::middleware(['auth:sanctum'])->prefix('apps')->group(function () {
   Route::get('unit-converter', [UnitConverterController::class, 'index']);
 });
